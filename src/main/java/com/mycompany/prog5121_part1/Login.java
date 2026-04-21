e32c882d0acccb4356acf15a7a16e0d0a954693e
lab_services_student <lab_services_student@LabVM2065688.hxf1etohn4hede0tw1gaub3uge.jnbx.internal.cloudapp.net>
2026-04-21 02:25:17 +0200

    Add Login class with constructor and fields

---
+++ src/main/java/com/mycompany/prog5121_part1/Login.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.prog5121_part1;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class Login { 
+    
+    private String name;
+    private String surname;
+    private String phonenumber;
+    private String username;
+    private String password;
+    
+    
+    public Login(String name, String surname, String phonenumber, String username, String password) {
+        this.name = name;
+        this.surname = surname;
+        this.phonenumber = phonenumber;
+        this.username = username;
+        this.password = password;
+               
+    }
+    
+            
+    
+}
---
+++ src/main/java/com/mycompany/prog5121_part1/Prog5121_part1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prog5121_part1;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class Prog5121_part1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
